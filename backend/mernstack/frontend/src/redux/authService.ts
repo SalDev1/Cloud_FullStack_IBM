da58e19da4ccab379df4c9e980e19a1de466ae0e
Salman Uddin <salmanuddin9820@gmail.com>
2024-05-05 21:01:21 +0530

    18th-commit

--- backend/mernstack/frontend/src/redux/authService.ts
+++ backend/mernstack/frontend/src/redux/authService.ts
@@ -5,7 +5,6 @@ const API_URL = 'http://localhost:9999/api/users/'
 // Register user
 const register = async (userData:any) => {
   const response = await axios.post(API_URL, userData)
-
   if (response.data) {
     localStorage.setItem('user', JSON.stringify(response.data))
   }
